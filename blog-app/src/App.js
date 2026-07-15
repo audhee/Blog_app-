0507b8dc742887ba2c7f29058e675369dd9614e4
audhee <audheeraj875@gmail.com>
2026-07-15 19:13:43 +0530

    Modified the app.js

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -3,10 +3,20 @@ import './App.css';
 import React from 'react';
 
 function App() {
+  const title =  `Welcome to the new blog`
+  const likes = 50;
+  const link = "http://www.google.com";
+  const person = { name:'yoshi', age:30}
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p> Liked { likes } times</p>
+        {/*<p>{person}</p>*/}
+        <p>{ person.name }</p>
+        <p>{ [1,2,3,4,5,6] }</p>
+        <p>{Math.random()*100}</p>
+        <a href={link}>Google it</a>
       </div>
     </div>
   );
